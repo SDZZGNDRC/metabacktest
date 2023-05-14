9401071d9b12477987a980bb302ea86ef5563774
SDZZGNDRC <705258871@qq.com>
2023-05-14 22:58:37 +0800

    build framework

---
+++ README.md
@@ -0,0 +1,17 @@
+# Metabacktest
+This project will generate various test for testing the back-test frameworks, such as `pybacktest` and `cbacktest`.  
+**NOTICE: Currently only supports for SPOT.**  
+# RUN
+> python main.py  
+# Process
+## Step 1
+1. 确定交易涉及到的交易对
+2. 确定策略balance中各个交易对的余额(注意, 交易中不涉及到的交易对也可能会有余额)
+3. 确定回测的时间区间[start, end], 时间粒度暂定为1 sec
+4. 确定在回测期间策略生成的指令(BUY/SELL, 撤单). 注意, 必须考虑到策略可能会生成'不正确'的指令, 比如在余额不足的情况下发出BUY指令; 指令中和价格相关的参数暂时空缺, 后续步骤补上, 包括但不限于: 限价, 交易量
+5. 生成策略生成的指令中涉及到的所有交易对的价格序列
+6. 根据`2`和`4`补充交易指令中的相关参数, 如限价, 交易量等
+7. 由`2`, `4`和`6`计算策略在回测过程中每一时间单元的balance
+8. 根据`2`,`4`和`7`生成订单簿
+## Step 2
+1. 将`Step 1`中生成的各种数据归类, 存储到指定文件中
---
+++ src/utils/instruments.py
@@ -0,0 +1,38 @@
+from typing import List
+import json
+import sys
+
+'''
+命令行参数: 原始数据的路径, 目标文件的路径
+该脚本主要用于将原始的GET /api/v5/public/instruments?instId=SPOT数据提取特定的字段到指定的文件
+'''
+
+def extract(filePath: str, toPath = None) -> List[dict]:
+    # Read json file
+    with open(filePath, 'r', encoding='utf-8') as f:
+        json_data = f.read()
+    data_dict = json.loads(json_data)
+    instruments = data_dict['data']
+    result : List[dict] = []
+    for i in instruments:
+        d = {}
+        d['instId'] = i['instId']
+        d['baseCcy'] = i['baseCcy']
+        d['quoteCcy'] = i['quoteCcy']
+        d['listTime'] = int(i['listTime'])
+        d['tickSz'] = i['tickSz']
+        d['lotSz'] = i['lotSz']
+        d['minSz'] = i['minSz']
+        d['state'] = i['state']
+        result.append(d)
+    if toPath == None:
+        final_data = {'instruments': result}
+        with open(toPath, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(final_data, indent=4))
+    return result
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        extract(sys.argv[1], sys.argv[2])
+
+        
